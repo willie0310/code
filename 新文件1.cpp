664999843167295d724796b4553b90e840ecb90f
willie0310 <willie0310@gamil.com>
2020-06-01 19:30:06 +0800

    multi

--- 新文件1.cpp
+++ 新文件1.cpp
@@ -1,10 +1,32 @@
 #include<iostream>
 using namespace std;
 
+
+int min(int x,int y)
+{
+	int c
+	c=x-y
+	return c;
+}
+
+int plu(int x,int y)
+{
+	int c
+	c=x+y
+	return c;
+}
+
+int mulit(int x,int y)
+{
+	int c
+	c=x*y
+	return c;
+}
+
 int main()
 {
 	int x,y;
 	cin>>x>>y;
-	
+	cout<<min(x,y)<<endl;
 	return 0;
  } 
